e52a9090143b116d56113c42980b8c06c631262a
Edward <127947259+edwardwoolls@users.noreply.github.com>
2024-12-04 16:21:19 +0400

    chore: initial commit

--- components/HelloWave.tsx
+++ components/HelloWave.tsx
@@ -16,9 +16,9 @@ export function HelloWave() {
   useEffect(() => {
     rotationAnimation.value = withRepeat(
       withSequence(withTiming(25, { duration: 150 }), withTiming(0, { duration: 150 })),
-      4 // Run the animation 4 times
+      4,
     );
-  }, []);
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   const animatedStyle = useAnimatedStyle(() => ({
     transform: [{ rotate: `${rotationAnimation.value}deg` }],
